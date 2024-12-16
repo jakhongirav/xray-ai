af5257473ed7a8b252c29d1deebd204c604da117
Jahongir Abdujalilov <jabdujalilov24@fed.idserve.net>
2024-12-16 15:53:33 +0100

    UI bug fixes

---
+++ xray-ai/Views/ScanView.swift
@@ -0,0 +1,18 @@
+//
+//  ScanView.swift
+//  xray-ai
+//
+//  Created by Jahongir Abdujalilov on 16/12/24.
+//
+
+import SwiftUI
+
+struct ScanView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScanView()
+}
